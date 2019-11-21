8448a55aaf36d218e6df8b74b4ad9625d23699f4
vagrant <vagrant@localhost.localdomain>
2019-11-21 03:14:53 +0000

    New lighting control loop

--- examples/adrian.py
+++ examples/adrian.py
@@ -7,7 +7,7 @@ from pytradfri.api.libcoap_api import APIFactory
 from pytradfri.error import PytradfriError
 from pytradfri.util import load_json, save_json
 
-DELAY_SECONDS = 1
+DELAY_SECONDS = 0.5
 CONFIG_FILE = "/home/vagrant/ikea/tradfri_standalone_psk.conf"
 
 host = "192.168.10.44"
@@ -33,12 +33,19 @@ print(lights)
 for _ in range(10):
     dim_command = lights[1].light_control.set_dimmer(0)
     api(dim_command)
-    dim_command = lights[2].light_control.set_dimmer(254)
+    dim_command = lights[2].light_control.set_dimmer(0)
     api(dim_command)
     time.sleep(DELAY_SECONDS)
 
     dim_command = lights[1].light_control.set_dimmer(254)
     api(dim_command)
-    dim_command = lights[2].light_control.set_dimmer(0)
+    dim_command = lights[2].light_control.set_dimmer(254)
     api(dim_command)
     time.sleep(DELAY_SECONDS)
+
+for i in range(0, 255, 5):
+    dim_command = lights[1].light_control.set_dimmer(i)
+    api(dim_command)
+    dim_command = lights[2].light_control.set_dimmer(i)
+    api(dim_command)
+    time.sleep(0.1)
